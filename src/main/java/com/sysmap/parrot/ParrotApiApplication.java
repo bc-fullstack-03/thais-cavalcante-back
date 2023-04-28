a4ca6a923969a29938bbdfe9daed79773aab50f2
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-04-28 18:58:59 -0300

    initializing project

---
+++ src/main/java/com/sysmap/parrot/ParrotApiApplication.java
@@ -0,0 +1,13 @@
+package com.sysmap.parrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrotApiApplication.class, args);
+	}
+
+}
