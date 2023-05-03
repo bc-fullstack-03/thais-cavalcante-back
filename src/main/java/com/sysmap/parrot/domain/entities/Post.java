9ea6d23ec657dbd732540487e81fe8afeeece41c
Thais Cavalcante <thaispcavalcante@gmail.com>
2023-05-02 22:07:56 -0300

    post entity and embedded comment class

---
+++ src/main/java/com/sysmap/parrot/domain/entities/Post.java
@@ -0,0 +1,43 @@
+package com.sysmap.parrot.domain.entities;
+
+import com.sysmap.parrot.domain.embedded.Comment;
+import com.sysmap.parrot.domain.embedded.Like;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class Post {
+    private UUID id;
+    private String content;
+    private String image;
+    private Date createdAt;
+    private UUID authorId;
+    private List<Comment> comments;
+    private List<Like> likes;
+
+    public Post (String content, String image, UUID authorId) {
+        this.setId();
+        this.createdAt = getCurrentDateTime();
+        this.content = content;
+        this.image = image;
+        this.authorId = authorId;
+        this.comments = new ArrayList<>();
+        this.likes = new ArrayList<>();
+    }
+
+    private Date getCurrentDateTime() {
+        return new Date();
+    }
+
+    protected void setId() {
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+}
